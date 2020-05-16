eba0eb39f4a3d1cc3991f1d441074334bb94c523
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-16 17:08:36 +0800

    start, midle , end ok

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const app = express()
 const port = 80
 const host = '0.0.0.0'
 
-app.get('/things/:name/:id', function(req, res) {
-    res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+//First middleware before response is sent
+app.use(function(req, res, next){
+    console.log("Start");
+    next();
  });
-//Other routes here
-app.get('*', function(req, res){
-    res.send('Sorry, this is an invalid URL.');
+ 
+ //Route handler
+ app.get('/', function(req, res, next){
+    res.send("Middle");
+    next();
+ });
+ 
+ app.use('/', function(req, res){
+    console.log('End');
  });
 app.listen(port, host,() => console.log(`Example app listening at http://${host}:${port}`))
