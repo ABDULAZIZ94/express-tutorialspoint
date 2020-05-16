6f20e753112c4b42c93556afba4aa8086b99817c
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-16 17:04:50 +0800

    404 ok

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ const host = '0.0.0.0'
 app.get('/things/:name/:id', function(req, res) {
     res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
  });
-
+//Other routes here
+app.get('*', function(req, res){
+    res.send('Sorry, this is an invalid URL.');
+ });
 app.listen(port, host,() => console.log(`Example app listening at http://${host}:${port}`))
