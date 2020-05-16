636c41f9fc74afe458a5663bbc8788b1b9d48dd9
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-16 17:02:31 +0800

    :name and :id ok

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 80
 const host = '0.0.0.0'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/things/:name/:id', function(req, res) {
+    res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+ });
 
 app.listen(port, host,() => console.log(`Example app listening at http://${host}:${port}`))
